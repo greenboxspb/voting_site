e3bc29aae6ba3c451ba52fcdd400431ed8b63bc5
greenboxspb <green.box.spb@gmail.com>
2018-03-02 11:26:49 +0300

    first commit

---
+++ votingvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/VictoriaKozhevnikova/Desktop/voting_site/votingvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
